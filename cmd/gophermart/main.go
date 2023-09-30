4fb6db9caceb7d4bdfbf61611e3a92b4590ac877
PolyaevMA <polyaev.maks@ya.ru>
2023-09-30 21:54:22 +0300

    add config

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/MaximPolyaev/gofermart/internal/config"
+)
+
+func main() {
+	cfg := config.New()
+	if err := cfg.Parse(); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/MaximPolyaev/gofermart
 
 go 1.21
+
+require (
+	github.com/caarlos0/env/v9 v9.0.0
+	github.com/joho/godotenv v1.5.1
+)
