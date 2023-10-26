e2411ca053ebe9cf9571dbe0be400e4b8613eeca
PolyaevMA <polyaev.maks@ya.ru>
2023-10-27 01:01:58 +0300

    user use case test

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/caarlos0/env/v9 v9.0.0
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/golang/mock v1.6.0
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
---
+++ internal/usecases/userusecase/user_test.go
@@ -0,0 +1,27 @@
+package userusecase
+
+import (
+	"context"
+	"github.com/MaximPolyaev/gofermart/internal/usecases/userusecase/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUserUseCase_GetUserIDByLogin(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mocks.NewMockstorage(ctrl)
+	ctx := context.TODO()
+
+	wantUserID := 1
+
+	store.EXPECT().FindUserIDByLogin(ctx, "test").Return(wantUserID, nil)
+
+	uc := New(store)
+	got, err := uc.GetUserIDByLogin(ctx, "test")
+
+	assert.NoError(t, err)
+	assert.Equal(t, wantUserID, got)
+}
